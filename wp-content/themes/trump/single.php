a7a60f5f252ab1c5950508c758107188fc56982d
Stanyslas Fruchard <stanyslas.fruchard@etu-webschoolfactory.fr>
2016-11-18 11:46:18 +0100

    Correction image méta single

--- wp-content/themes/trump/single.php
+++ wp-content/themes/trump/single.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-    
+
     <?php if ( have_posts() ) : ?>
             <?php while ( have_posts() ) : the_post(); ?>
 
@@ -9,13 +9,13 @@
                 <meta property="og:url" content="<?php the_permalink() ?>" />
                 <meta property="og:title" content="Trump said it..." />
                 <meta property="og:description" content="<?php the_title(); ?>" />
-                <meta property="og:image" content="http://127.0.0.1/projects/Trumpatations/wordpress/wp-content/uploads/2016/11/0620trumppolicies01.png" />
-                
+                <meta property="og:image" content="/wp-content/uploads/2016/11/0620trumppolicies01.png" />
+
                 <meta property="og:url"           content="<?php the_permalink() ?>" />
                 <meta property="og:type"          content="website" />
                 <meta property="og:title"         content="Trump said it..." />
                 <meta property="og:description"   content="<?php the_title(); ?>" />
-                <meta property="og:image"         content="http://127.0.0.1/projects/Trumpatations/wordpress/wp-content/uploads/2016/11/0620trumppolicies01.png" />
+                <meta property="og:image"         content="/wp-content/uploads/2016/11/0620trumppolicies01.png" />
 
                 <div id="main" role="main" class="mod pam">
                     <h2><?php the_title(); ?></h2>
@@ -33,4 +33,3 @@
 </div>
 
 <?php get_footer();
-		
